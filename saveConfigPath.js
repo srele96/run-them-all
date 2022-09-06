44f9d06321d476b899790ef86e7929fab5f79e95
Srecko <srele96@gmail.com>
2022-09-06 20:17:08 +0200

    save path to a configuration

---
+++ saveConfigPath.js
@@ -0,0 +1,35 @@
+const { extname, resolve } = require('path');
+const { writeFile } = require('fs');
+
+/**
+ * Overwrites CLI configuration and saves users path.
+ *
+ * @param {string} path relative or absolute configuration path
+ */
+module.exports.saveConfigPath = function saveConfigPath(path) {
+  if (typeof path !== 'string' || path === '')
+    throw new Error('Path must be a non-empty string!');
+
+  const supportedExtension = 'json';
+  const isSupported = extname(path) === supportedExtension;
+
+  if (isSupported) {
+    const createAbsolutePath = resolve(path);
+    const createConfig = { path: createAbsolutePath };
+
+    writeFile('config.json', createConfig, (err) => {
+      if (err)
+        console.log('Failed to save path!\n', JSON.stringify(err, null, 2));
+      else
+        console.log(
+          'Saved path successfully!\n' + JSON.stringify(createConfig, null, 2)
+        );
+    });
+  } else
+    throw new Error(
+      "Couldn't save your configuration: " +
+        path +
+        '. Currently supported configurations are: ' +
+        supportedExtension.toUpperCase()
+    );
+};
